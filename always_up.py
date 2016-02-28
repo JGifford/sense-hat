9b1f0987f2a0577a039f69a058352748ae42b848
Jeffrey Gifford <jgifford@sojourner.local>
2016-02-28 08:25:04 -0500

    Added comments

--- always_up.py
+++ always_up.py
@@ -27,23 +27,20 @@ arrow = [
 ]
 
 red_arrow = [
-e,e,e,r,r,e,e,e,
-e,e,r,r,r,r,e,e,
-e,r,e,r,r,e,r,e,
-r,e,e,r,r,e,e,r,
-e,e,e,r,r,e,e,e,
-e,e,e,r,r,e,e,e,
-e,e,e,r,r,e,e,e,
-e,e,e,r,r,e,e,e
+    e, e, e, r, r, e, e, e,
+    e, e, r, r, r, r, e, e,
+    e, r, e, r, r, e, r, e,
+    r, e, e, r, r, e, e, r,
+    e, e, e, r, r, e, e, e,
+    e, e, e, r, r, e, e, e,
+    e, e, e, r, r, e, e, e,
+    e, e, e, r, r, e, e, e
 ]
 
 # Print it once
-#sh.set_pixels(arrow)
+sh.set_pixels(arrow)
 
 while True:
-    # Below works with Rasbian's Python 2.7.9
-    # x_full, y_full, z_full = sh.get_accelerometer_raw().values()
-
     # Below works with Rasbian's Python 3.4.2 AND 2.7.9
     raw = sh.get_accelerometer_raw()
 
@@ -52,24 +49,20 @@ while True:
     z=round(raw['z'], 0)
 
     # Not really needed, except for perhaps debugging
-    # Below works with Rasbian's Python 2.7.9
-    #print ("x=%s, y=%s, z=%s" % (x_full,y_full,z_full))
-
     # Below works with Rasbian's Python 3.4.2 AND 2.7.9
     #print ("x=%s, y=%s, z=%s" % (raw['x'],raw['y'],raw['z']))
 
+    # Depending on which x or y axis is pointing up or down
+    # Rotate the "arrow" (set_pixels, above) to point up
+    # Assumes earth-gravity
     if x == -1:
-        sh.set_pixels(arrow)
         sh.set_rotation(90)
     elif x == 1:
-        sh.set_pixels(red_arrow)
         sh.set_rotation(270)
 
     elif y == 1:
-        sh.set_pixels(arrow)
         sh.set_rotation(0)
     elif y == -1:
-        sh.set_pixels(red_arrow)
         sh.set_rotation(180)
     else:
         sh.show_letter("*")
